280e9e860ef8d4ecc765bac3ba5c19ab81053fec
Aapelii <aapeli.vaisanen@gmail.com>
2023-09-10 18:19:12 +0300

    hmmm

--- App.js
+++ App.js
@@ -1,49 +1,14 @@
 import {StatusBar} from 'expo-status-bar';
-import {Platform} from 'react-native';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import List from './components/List';
+import Navigator from './navigators/navigator';
+
 
 const App = () => {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <List />
-        <StatusBar style="auto" />
-      </SafeAreaView>
+      <Navigator/>
+      <StatusBar style="auto" />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-    backgroundColor: '#87CEFA',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    flex: 1,
-  },
-  TouchableOpacity: {
-    marginVertical: 5,
-  },
-  box: {
-    flexDirection: 'row',
-    backgroundColor: '#6495ED',
-    alignItems: 'center',
-    paddingHorizontal: 10,
-  },
-  Image: {
-    width: '85%',
-    height: 'auto',
-    resizeMode: 'cover',
-  },
-  View: {
-    width: '50%',
-    margin: 5,
-  },
-  Text: {
-    height: 'auto',
-    width: '100%',
-  },
-});
 
 export default App;
